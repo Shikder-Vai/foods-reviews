20fa8554c193d9c14176b264470a381867db3131
unknown <Shikder-Vai>
2022-04-05 18:08:39 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Error from "./components/Error/Error";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Reviews from "./components/Reviews/Reviews";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Error></Error>}></Route>
       </Routes>
     </div>
   );
